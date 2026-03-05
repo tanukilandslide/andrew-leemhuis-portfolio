0184ef5e837ee6d2a87c225d83aa315e7e3f9c80
Andrew Leemhuis <tanukilandslide@gmail.com>
2026-03-05 22:16:37 +0900

    Added photos to the Loan Progress Simulator

--- src/pages/loan-progress-simulator/LoanProgressSimulator.vue
+++ src/pages/loan-progress-simulator/LoanProgressSimulator.vue
@@ -9,11 +9,16 @@
 
       <p>
         A tool to give Customer Service Agents and customers transparency into their loan payments.
-        The goal for this project was to make a tool that would go beyond a basic loan calculator and
-        give personalized information about how simple interest is calculated on a customer's loan.
+        The goal for this project was to make a tool that would go beyond a basic loan calculator
+        and give personalized information about how simple interest is calculated on a customer's
+        loan.
       </p>
 
       <h2>Don't want to read? Watch the case study I created for our World Design Summit!</h2>
+      <video width="100%" class="rounded-lg" controls>
+        <source src="./video/00-world-design-summit-case-study.mp4" type="video/mp4" />
+        Your browser doesn't support this video format.
+      </video>
     </Box>
 
     <SlidingBox>
@@ -30,7 +35,9 @@
 
     <SlidingBox>
       <h2>How customers think simple interest works:</h2>
+      <img src="./images/01_interest_perception.svg" alt="" class="w-4/5" />
       <h2>How simple interest actually works:</h2>
+      <img src="./images/02_interest_reality.svg" alt="" class="w-4/5" />
     </SlidingBox>
 
     <SlidingBox>
@@ -49,9 +56,12 @@
         conversations with customers calling about their loans. After listening to 70 calls, I
         compiled my notes to create 3 personas.
       </p>
+
+      <img src="./images/03_call-center.jpg" alt="" class="w-4/5" />
     </SlidingBox>
 
     <Box>
+      <img src="./images/04_early-stage-customer.png" alt="" class="w-4/5" />
       <h1>Primary Persona: Early Stage Delinquency Customer</h1>
 
       <p>
@@ -65,8 +75,8 @@
       <ul>
         <li>Wants to work towards getting back to current on their delinquent account</li>
         <li>
-          Wants agent to be empathetic to customer&#39;s condition and help find a way out of current
-          debt
+          Wants agent to be empathetic to customer&#39;s condition and help find a way out of
+          current debt
         </li>
         <li>Wants to understand their loan structure and look under the hood</li>
         <li>Wants to make sure that money has successfully been paid towards account</li>
@@ -77,18 +87,20 @@
 
       <ul>
         <li>
-          Thinks that there is only a fee for paying late, doesn&#39;t know that interest also accrues
+          Thinks that there is only a fee for paying late, doesn&#39;t know that interest also
+          accrues
         </li>
         <li>
-          Will want to pay late fees on current payment, but late fees will roll into next month&#39;s
-          payment
+          Will want to pay late fees on current payment, but late fees will roll into next
+          month&#39;s payment
         </li>
         <li>Doesn&#39;t have the level of transparency into their loan as they might like</li>
       </ul>
     </Box>
 
     <Box>
-      <h1 class="persona_h1">Secondary Persona: Proactive Customer</h1>
+      <img src="./images/05_proactive-customer.png" alt="" class="w-4/5" />
+      <h1>Secondary Persona: Proactive Customer</h1>
 
       <p>
         This customer always pays regularly and wants to better track the progress of their loan.
@@ -106,16 +118,19 @@
       <h2>Frustrations</h2>
 
       <ul>
-        <li>Doesn&#39;t have a tool outside of calling customer service to find that information</li>
+        <li>
+          Doesn&#39;t have a tool outside of calling customer service to find that information
+        </li>
       </ul>
     </Box>
 
     <Box>
-      <h1 class="persona_h1">Customer Service Agent</h1>
+      <img src="./images/06_customer-service-agent.png" alt="" class="w-4/5" />
+      <h1>Customer Service Agent</h1>
 
       <p>
-        The Customer Service Representative fields all of the general calls for Santander and Chrysler
-        Capital. The majority of calls are about payment questions.
+        The Customer Service Representative fields all of the general calls for Santander and
+        Chrysler Capital. The majority of calls are about payment questions.
       </p>
 
       <h2>Goals</h2>
@@ -125,8 +140,8 @@
         <li>Tries to answer calls in 6 minutes or less</li>
         <li>Has to hit all areas of script before hanging up with customer</li>
         <li>
-          60% of calls are about payments, 20% of calls are about titles, 15% are about insurance, and
-          5% need to be escalated
+          60% of calls are about payments, 20% of calls are about titles, 15% are about insurance,
+          and 5% need to be escalated
         </li>
       </ul>
 
@@ -138,21 +153,24 @@
           to learn how interest is actually calculated.
         </li>
         <li>
-          Agents will have many windows open for different types of calls they may receive, cluttering
-          their computer screen
+          Agents will have many windows open for different types of calls they may receive,
+          cluttering their computer screen
         </li>
       </ul>
     </Box>
 
-    <SlidingBox> </SlidingBox>
+    <SlidingBox>
+      <img src="./images/07_analytics.svg" alt="" class="w-4/5" />
+    </SlidingBox>
 
     <Box>
       <h2>First Prototype</h2>
+      <img src="./images/08_first-prototype.jpg" alt="" class="w-4/5" />
       <h2>Usability Test Results</h2>
 
       <p>
-        Working with our UX researcher, we tested this wireframe with 6 customer service, early stage,
-        and late stage collections agents.
+        Working with our UX researcher, we tested this wireframe with 6 customer service, early
+        stage, and late stage collections agents.
       </p>
 
       <ul>
@@ -167,12 +185,11 @@
 
     <Box>
       <h2>Second Prototype</h2>
-
-      <!-- <img
-        src="images/09_second-prototype.jpg"
-        class="image"
+      <img
+        src="./images/09_second-prototype.jpg"
         alt="A user interface with bars indicating the amount of the loan paid off, a graph showing payments, and one indicating a timeline of loan events"
-      /> -->
+        class="w-4/5"
+      />
 
       <h2>Usability Test Results</h2>
 
@@ -190,10 +207,10 @@
 
     <SlidingBox>
       <h2>What&apos;s this prototype look like?</h2>
-      <!-- <video width="100%" controls>
-        <source src="video/01-lps-demonstration.mp4" type="video/mp4" />
+      <video width="100%" class="rounded-lg" controls>
+        <source src="./video/01-lps-demonstration.mp4" type="video/mp4" />
         Your browser doesn't support this video format.
-      </video> -->
+      </video>
     </SlidingBox>
 
     <Button>
@@ -219,5 +236,5 @@
 <script setup>
 import Box from '@/components/Box.vue'
 import Button from '@/components/Button.vue'
-import SlidingBox from '@/components/SlidingBox.vue';
+import SlidingBox from '@/components/SlidingBox.vue'
 </script>
