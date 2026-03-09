8a8592b94df3000edd23c60dedcfa50ddb068328
Andrew Leemhuis <tanukilandslide@gmail.com>
2026-03-09 20:25:14 +0900

    Image gallery is now fully functional and switches both projects and images

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,10 +1,14 @@
 <template>
   <h2>Image Gallery</h2>
   <div class="flex w-full gap-3">
-    <img :src="`/src/pages/personal-projects/images/${heroImage}`" alt="" class="h-fit" />
-    <div class="flex-col h-fit">
+    <img
+      :src="`/src/pages/personal-projects/images/${heroImage}`"
+      alt=""
+      class="w-2/3 object-scale-down"
+    />
+    <div class="flex-col w-1/3">
       <img
-        v-for="image in images"
+        v-for="image in images.imageSet"
         :src="`/src/pages/personal-projects/images/${image}`"
         alt=""
         :key="image"
@@ -16,37 +20,26 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, defineProps, watch } from 'vue'
 
-const images = [
-  'chair_project/01-initial-chair-sketches.jpg',
-  'chair_project/02-chairs-color.jpg',
-  'chair_project/03-chair-construction.jpg',
-  'chair_project/04-process-photos.jpg',
-  'chair_project/05-final-chair.jpg',
-]
+let images = defineProps({
+  imageSet: {
+    type: Array,
+  },
+})
 
-let heroImage = ref(images[0])
+let heroImage = ref(images.imageSet[0])
+
+console.log(images.imageSet[0])
 
 function imageChange(image) {
   console.log(image)
   heroImage.value = image
 }
 
-// const images = [
-//   'flower_pot/01_initial_sketch.jpg',
-//   'flower_pot/02_3d_model_rendering.jpg',
-//   'flower_pot/03_flower_pot_printing.gif',
-//   'flower_pot/04_flower_pot_on_fence.jpg',
-//   'flower_pot/05_flower_pot_with_plant.jpg',
-// ]
-
-// const images = [
-//   'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.YFmJZ1hmnsbXTGc7zCaiPgHaE8%3Fpid%3DApi&f=1&ipt=9fad29e18c046d28b885b52d349dd8f5cb206edf4dcde3ea06b64c9774ad9930&ipo=images',
-//   'https://dailyjugarr.com/wp-content/uploads/2024/07/Farewell_-Furry-Friend-How-My-Cat-Taught-Me-the-True-Meaning-of-Love.webp',
-// ]
-
-console.log(images)
+watch(images, (newImage) => {
+  imageChange(newImage.imageSet[0])
+})
 
 // const files = require('@/pages')
 // console.log(files)
--- src/pages/personal-projects/PersonalProjects.vue
+++ src/pages/personal-projects/PersonalProjects.vue
@@ -1,10 +1,68 @@
 <template>
   <main class="max-w-[980px]">
     <h1>Personal Projects</h1>
-    <ImageGallery></ImageGallery>
+    <ImageGallery :image-set="currentProject.images"></ImageGallery>
+    <div class="flex justify-between">
+      <div
+        v-for="project in projects"
+        :key="project"
+        class="cursor-pointer"
+        @click="setProject(project)"
+      >
+        <h3>{{ project.name }}</h3>
+        <img
+          :src="`/src/pages/personal-projects/images/${project.thumbnail}`"
+          alt=""
+          class="rounded-full"
+        />
+      </div>
+    </div>
   </main>
 </template>
 
 <script setup>
 import ImageGallery from '@/components/ImageGallery.vue'
+import { ref } from 'vue'
+
+const projects = [
+  {
+    name: 'Breathe Chair',
+    thumbnail: 'chair_project/00-chair-icon.jpg',
+    images: [
+      'chair_project/01-initial-chair-sketches.jpg',
+      'chair_project/02-chairs-color.jpg',
+      'chair_project/03-chair-construction.jpg',
+      'chair_project/04-process-photos.jpg',
+      'chair_project/05-final-chair.jpg',
+    ],
+  },
+  {
+    name: 'Fence Flower Pot',
+    thumbnail: 'flower_pot/00_flower-pot-icon.jpg',
+    images: [
+      'flower_pot/01_initial_sketch.jpg',
+      'flower_pot/02_3d_model_rendering.jpg',
+      'flower_pot/03_flower_pot_printing.gif',
+      'flower_pot/04_flower_pot_on_fence.jpg',
+      'flower_pot/05_flower_pot_with_plant.jpg',
+    ],
+  },
+  {
+    name: 'Sketches',
+    thumbnail: 'sketches/00-fringilla-icon.jpg',
+    images: [
+      'sketches/01-fringilla.jpg',
+      'sketches/02-leighton-on-a-mountain.jpg',
+      'sketches/03-sci-fi-helmet.jpg',
+      'sketches/04-helmet.jpg',
+      'sketches/05-pottery-throwdown-pickup.jpg',
+    ],
+  },
+]
+
+let currentProject = ref(projects[0])
+
+function setProject(project) {
+  currentProject.value = project
+}
 </script>
