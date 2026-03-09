846daf5d4cd20006fca06fac245709d44d4ca013
Andrew Leemhuis <tanukilandslide@gmail.com>
2026-03-09 16:10:38 +0900

    Images added to Links Visual Style Development

--- src/pages/linkshme-website/LinksVisualStyleDevelopment.vue
+++ src/pages/linkshme-website/LinksVisualStyleDevelopment.vue
@@ -9,6 +9,15 @@
         disingenuous, I settled on meeples to represent patients and the people who work at HME
         companies.
       </p>
+
+      <div class="flex">
+        <img src="./images/13_bebbeh.jpg" alt="" />
+        <img src="./images/14_bebbeh_vector.jpg" alt="" />
+        <img src="./images/15_iv_stand.jpg" alt="" />
+        <img src="./images/16_iv_stand_vector.jpg" alt="" />
+        <img src="./images/17_kimberlin.jpg" alt="" />
+        <img src="./images/18_kimberlin_vector.jpg" alt="" />
+      </div>
     </SlidingBox>
 
     <Box>
@@ -18,16 +27,28 @@
         through the visual design of our website by creating illustrations for our features pages.
       </p>
       <h3 class="">Customer Center Portal</h3>
+      <img src="./images/19_feature-banner-customer-center-portal.png" alt="" />
+      <img src="./images/20_feature-banner-customer-center-portal_sketch.jpg" alt="" />
 
       <h3 class="">Integration with Vendor Catalogs</h3>
+      <img src="./images/21_feature-banner-integration-with-vendor-catalogs.png" alt="" />
+      <img src="./images/22_feature-banner-integration-with-vendor-catalogs_sketch.jpg" alt="" />
 
       <h3 class="">Fulfillment Service Integration</h3>
+      <img src="./images/23_feature-banner-fulfillment-service-integration.png" alt="" />
+      <img src="./images/24_feature-banner-fulfillment-service-integration_sketch.jpg" alt="" />
 
       <h3 class="">Conversion</h3>
+      <img src="./images/25_feature-banner-conversion.png" alt="" />
+      <img src="./images/26_feature-banner-conversion_sketch.jpg" alt="" />
 
       <h3 class="">Cloud Connectivity</h3>
+      <img src="./images/27_feature-banner-cloud-connectivity.png" alt="" />
+      <img src="./images/28_feature-banner-cloud-connectivity_sketch.jpg" alt="" />
 
       <h3 class="text_content">Real Time Billing</h3>
+      <img src="./images/29_feature-banner-automatic-real-time-billing.png" alt="" />
+      <img src="./images/30_feature-banner-automatic-real-time-billing_sketch.jpg" alt="" />
     </Box>
 
     <Box>
@@ -42,9 +63,10 @@
         I also asked them several questions as well. The results are as follows:
       </p>
 
+      <img src="./images/31_re-testing.jpg" alt="" />
       <h3 class="links_header">Top Adjective Results</h3>
-      <div class="adjectives_and_numbers">
-        <ul class="adjective_ul">
+      <div class="flex">
+        <ul class="adjective_ul pe-3">
           <li class="adjective_li">Friendly</li>
           <li class="adjective_li">Easy To Use</li>
           <li class="adjective_li">Useful</li>
@@ -82,7 +104,10 @@
       </ul>
     </Box>
 
-    <Box> </Box>
+    <Box>
+      <img src="./images/32_homepage_first_draft.jpg" alt="" />
+      <img src="./images/33_homepage_final.jpg" alt="" />
+    </Box>
   </main>
 
   <Button>
