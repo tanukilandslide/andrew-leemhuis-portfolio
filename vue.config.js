0956d0a7c32faf959a6d3ae4b6952f6408f716b3
Andrew Leemhuis <tanukilandslide@gmail.com>
2026-07-21 01:20:52 +0900

    Testing out public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
+  publicPath: { process.env.NODE_ENV === 'production' ? '/public/' : '/' },
     configureWebpack: {
         resolve: {
             symlinks: false,
